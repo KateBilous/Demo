882729467599c5a614818b8f42fdb2e17daf23dd
KateBilous <katebilous@outlook.com>
2020-06-16 17:33:03 +0300

    Task2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,57 +1,38 @@
 package ru.stqa.pft.sandbox;
 
 
+public class Point {
+    public double x1;
+    public double x2;
+    public double y1;
+    public double y2;
+
+    public Point(double x1, double x2, double y1, double y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+
+    }
 
 
-    public class Point {
-        public  double x1;
-        public  double x2;
-        public double y1;
-        public double y2;
-
-        public Point(){
-            
-        }
+    public static void main(String[] args) {
 
 
-
-        public static void main(String[] args) {
-
-
-
-        /*Point1 p1 = new Point1(9,9, null,null);
-        Point1 p2 = new Point1(null,null,8,5);*/
-
-            Point p1 =new Point();
-            p1.x1= 9;
-            p1.x2 =6;
-            Point p2 = new Point();
-            p2.y1 =-4;
-            p2.y2 =0;
-
-            System.out.println("Distance between two points are "+ distance(p1,p2));
+        Point distance = new Point(5, 7, 6, 9);
+        System.out.println("Distance between two points are " + distance.distance());
 
 
+    }
+
+    public double distance() {
+
+        double ac = Math.abs(this.x1 - this.x2);
+        double cb = Math.abs(this.y1 - this.y2);
+        return Math.hypot(ac, cb);
 
 
-
-        }   public static double distance(Point p1, Point p2){
-
-            double ac = Math.abs(p1.x1 - p1.x2);
-            double cb = Math.abs(p2.y1 - p2.y2);
-            return Math.hypot(ac, cb) ;
-
-
-        }
-
-
-
-
-
-
-
-
-
+    }
 
 
 }
